892984d5b3fc00e5eeb7ece520384c5b2def5871
anushkadhiman <54269874+anushkadhiman@users.noreply.github.com>
2020-10-15 18:52:18 +0530

    Update configs.py

--- yolov3/configs.py
+++ yolov3/configs.py
@@ -1,5 +1,5 @@
 # YOLO options
-YOLO_TYPE                   = "yolov3" # yolov4 or yolov3
+YOLO_TYPE                   = "yolov3"
 YOLO_FRAMEWORK              = "tf" # "tf" or "trt"
 YOLO_V3_WEIGHTS             = "model_data/yolov3.weights"
 YOLO_V3_TINY_WEIGHTS        = "model_data/yolov3-tiny.weights"
@@ -12,10 +12,9 @@ YOLO_IOU_LOSS_THRESH        = 0.5
 YOLO_ANCHOR_PER_SCALE       = 3
 YOLO_MAX_BBOX_PER_SCALE     = 100
 YOLO_INPUT_SIZE             = 416
-if YOLO_TYPE                == "yolov3":
-    YOLO_ANCHORS            = [[[10,  13], [16,   30], [33,   23]],
-                               [[30,  61], [62,   45], [59,  119]],
-                               [[116, 90], [156, 198], [373, 326]]]
+YOLO_ANCHORS            = [[[10,  13], [16,   30], [33,   23]],
+                           [[30,  61], [62,   45], [59,  119]],
+                           [[116, 90], [156, 198], [373, 326]]]
 # Train options
 TRAIN_YOLO_TINY             = False
 TRAIN_SAVE_BEST_ONLY        = True # saves only best model according validation loss (True recommended)
